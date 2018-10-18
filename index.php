e1c92bde5b39a9ffaefd29ba019f7875f129869c
nabilahsan <nabilahsan95@gmail.com>
2018-10-18 15:50:43 -0500

    displayed query 10 from project 2
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $query1 = "SELECT * FROM Customer";
 $result = mysqli_query($conn, $query1);
 $check = mysqli_num_rows($result);
 
+//Running query 1
 if ($check > 0) {
     # code...
     echo "<table border='1'>
@@ -26,6 +27,31 @@ if ($check > 0) {
         echo '<tr>';
         echo '<td>'.$row[c_id].'</td>';
         echo '<td>'.$row[c_name].'</td>';
+        echo '</tr>';
+    }
+    echo "</table>";
+}
+
+$query2 = "SELECT C.c_state AS State, count(*) AS CustormerCount FROM Customer C GROUP BY C.c_state;";
+
+$result2 = mysqli_query($conn, $query2);
+$check2 = mysqli_num_rows($result2);
+
+//Running query 2
+if ($check2 > 0) {
+    # code...
+    echo '<hr>';
+    echo 'Running query 10';
+    echo "<table border='1'>
+                <tr>
+                <th>STATE</th>
+                <th>CUSTOMER COUNT</th>
+                </tr>";
+    while ($row = mysqli_fetch_array($result2)) {
+        # code...
+        echo '<tr>';
+        echo '<td>'.$row[State].'</td>';
+        echo '<td>'.$row[CustormerCount].'</td>';
         echo '</tr>';
     }
     echo "</table>";
