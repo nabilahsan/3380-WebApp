23877d0251fd9907894782a3f823fb693ddb3fee
nabilahsan <nabilahsan95@gmail.com>
2018-10-18 12:56:09 -0500

    index has customer stuff
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
+$servername = "localhost";
 
-echo '<h1>DATABASE</h1>';
-include 'customer.php';
+$username = "user2";
 
+$password = "Hershey@2018";
+
+$db = "webapp";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+$query1 = "SELECT * FROM Customer";
+$result = mysqli_query($conn, $query1);
+$check = mysqli_num_rows($result);
+
+if ($check > 0) {
+    # code...
+    echo "<table border='1'>
+                <tr>
+                <th>ID</th>
+                <th>Name</th>
+                </tr>";
+    while ($row = mysqli_fetch_array($result)) {
+        # code...
+        echo '<tr>';
+        echo '<td>'.$row[c_id].'</td>';
+        echo '<td>'.$row[c_name].'</td>';
+        echo '</tr>';
+    }
+    echo "</table>";
+}
 ?>
