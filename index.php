9bce6369d843a9d76650b867ff5ed913caa634fb
nabilahsan <nabilahsan95@gmail.com>
2018-11-01 23:05:30 -0500

    displays styles output for procedure
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- index.php
+++ index.php
@@ -10,76 +10,58 @@ $db = "webapp";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
 
-$query1 = "SELECT * FROM Customer";
-$result = mysqli_query($conn, $query1);
-$check = mysqli_num_rows($result);
+?>
 
-//Running query 1
-if ($check > 0) {
-    # code...
-    echo "<table border='1'>
-                <tr>
-                <th>ID</th>
-                <th>Name</th>
-                </tr>";
-    while ($row = mysqli_fetch_array($result)) {
-        # code...
-        echo '<tr>';
-        echo '<td>'.$row[c_id].'</td>';
-        echo '<td>'.$row[c_name].'</td>';
-        echo '</tr>';
-    }
-    echo "</table>";
-}
+<html>
 
-$query2 = "SELECT C.c_state AS State, count(*) AS CustormerCount FROM Customer C GROUP BY C.c_state;";
+<head>
+    <title>PHP in HTML Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</head>
 
-$result2 = mysqli_query($conn, $query2);
-$check2 = mysqli_num_rows($result2);
-
-//Running query 2
-if ($check2 > 0) {
-    # code...
-    echo '<hr>';
-    echo 'Running query 10';
-    echo "<table border='1'>
-                <tr>
-                <th>STATE</th>
-                <th>CUSTOMER COUNT</th>
-                </tr>";
-    while ($row = mysqli_fetch_array($result2)) {
-        # code...
-        echo '<tr>';
-        echo '<td>'.$row[State].'</td>';
-        echo '<td>'.$row[CustormerCount].'</td>';
-        echo '</tr>';
-    }
-    echo "</table>";
-}
-
-//Stored procedures
-//Ref: http://www.mysqltutorial.org/php-calling-mysql-stored-procedures/
-
-$sp1 = "CALL `getProductName`()";
-
-$result3 = mysqli_query($conn, $sp1);
-$check3 = mysqli_num_rows($result3);
-
-if ($check3 > 0) {
-    # code...
-    echo '<hr>';
-    echo '<h2>Result of Stored Procedure 1: Getting Product Names</h2>';
-    echo "<table border='1'>
-                <tr>
-                <th>Product Name</th>
-                </tr>";
-    while ($row = mysqli_fetch_array($result3)) {
-        # code...
-        echo '<tr>';
-        echo '<td>'.$row[p_name].'</td>';
-        echo '</tr>';
-    }
-    echo "</table>";
-}
-
+</html>
+<?php
+    //Stored procedures
+    //Ref: http://www.mysqltutorial.org/php-calling-mysql-stored-procedures/
+    $sp1 = "CALL `ProductLineSale`()";
+    mysqli_query($conn, $sp1); // Initiate Procedure
+    $sp1 = "SELECT * FROM Product;";
+    $result3 = mysqli_query($conn, $sp1); // Get Product Contents
+    if (mysqli_num_rows($result3) > 0) {
+        echo '<hr>';
+        echo '<h2>Result of Stored Procedure 1: Products with new SalesPrice column</h2>';
+        echo "<table class='table table-striped'>
+                    <tr>
+                        <th scope='col'>ID</th>
+                        <th scope='col' >Name</th>
+                        <th scope='col'>Finish</th>
+                        <th scope='col'>Price</th>
+                        <th scope='col'>Product Line ID</th>
+                        <th scope='col'>Photo</th>
+                        <th scope='col'>SalePrice</th>
+                    </tr>";
+        while ($row = mysqli_fetch_array($result3)) {
+            # code...
+    /*
+            echo '<tr>';
+            echo '<td>'.$row['SalePrice'].'</td>'; // was giving me a warning in the browser
+            echo '</tr>';
+    */
+            echo '  <tr>
+                        <td>'.$row['p_id'].'</td>
+                        <td>'.$row['p_name'].'</td>
+                        <td>'.$row['p_finish'].'</td>
+                        <td>'.$row['p_standard_price'].'</td>
+                        <td>'.$row['pl_id'].'</td>
+                        <td>'.$row['p_photo'].'</td>
+                        <td>'.$row['SalePrice'].'</td>
+                    </tr>';
+    
+        }
+        echo "</table>";
+    } else
+        echo 'Nothing in table';
 ?>
